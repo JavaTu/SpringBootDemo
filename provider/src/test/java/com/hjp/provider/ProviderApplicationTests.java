8bdc4003de3a0906e0d2993cd61f225fea81ea3e
794926872@qq.com <dwhzsqj555>
2021-06-25 15:54:17 +0800

    增加RocketMQ。

--- provider/src/test/java/com/hjp/provider/ProviderApplicationTests.java
+++ provider/src/test/java/com/hjp/provider/ProviderApplicationTests.java
@@ -2,6 +2,7 @@ package com.hjp.provider;
 
 import com.hjp.provider.service.ProviderService;
 import org.apache.dubbo.config.annotation.Reference;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -14,6 +15,9 @@ class ProviderApplicationTests {
     @Reference(check = false, version = "1.0.0")
     private ProviderService providerService;
 
+    @Resource
+    private RocketMQTemplate template;
+
     @Test
     void contextLoads() {
         providerService.f();
@@ -22,5 +26,10 @@ class ProviderApplicationTests {
     @Test
     void testDubbo() {
         providerService.f();
+    }
+
+    @Test
+    void mq() {
+        template.convertAndSend("HJP_TOPIC", "HJP");
     }
 }
